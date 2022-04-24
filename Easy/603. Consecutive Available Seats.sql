ccc6a836327d5fdae3d052c3479424a974465def
ZihanWan <103607344+ZihanWan97@users.noreply.github.com>
2022-04-23 23:16:01 -0400

    Update 603. Consecutive Available Seats.sql

--- Easy/603. Consecutive Available Seats.sql
+++ Easy/603. Consecutive Available Seats.sql
@@ -1,46 +1,46 @@
-Table: Cinema
+-- Table: Cinema
 
-+-------------+------+
-| Column Name | Type |
-+-------------+------+
-| seat_id     | int  |
-| free        | bool |
-+-------------+------+
-seat_id is an auto-increment primary key column for this table.
-Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | seat_id     | int  |
+-- | free        | bool |
+-- +-------------+------+
+-- seat_id is an auto-increment primary key column for this table.
+-- Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
  
 
-Write an SQL query to report all the consecutive available seats in the cinema.
+-- Write an SQL query to report all the consecutive available seats in the cinema.
 
-Return the result table ordered by seat_id in ascending order.
+-- Return the result table ordered by seat_id in ascending order.
 
-The test cases are generated so that more than two seats are consecutively available.
+-- The test cases are generated so that more than two seats are consecutively available.
 
-The query result format is in the following example.
+-- The query result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Cinema table:
-+---------+------+
-| seat_id | free |
-+---------+------+
-| 1       | 1    |
-| 2       | 0    |
-| 3       | 1    |
-| 4       | 1    |
-| 5       | 1    |
-+---------+------+
-Output: 
-+---------+
-| seat_id |
-+---------+
-| 3       |
-| 4       |
-| 5       |
-+---------+
+-- Input: 
+-- Cinema table:
+-- +---------+------+
+-- | seat_id | free |
+-- +---------+------+
+-- | 1       | 1    |
+-- | 2       | 0    |
+-- | 3       | 1    |
+-- | 4       | 1    |
+-- | 5       | 1    |
+-- +---------+------+
+-- Output: 
+-- +---------+
+-- | seat_id |
+-- +---------+
+-- | 3       |
+-- | 4       |
+-- | 5       |
+-- +---------+
 
 
 
