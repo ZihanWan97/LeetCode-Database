29b9d3e0e84ee0a7f50755fc02efb13d6a9a01f4
ZihanWan <103607344+ZihanWan97@users.noreply.github.com>
2022-05-01 23:15:27 -0400

    Update 1565. Unique Orders and Customers Per Month.sql

--- Easy/1565. Unique Orders and Customers Per Month.sql
+++ Easy/1565. Unique Orders and Customers Per Month.sql
@@ -56,8 +56,8 @@
 
 
 
--- # Write your MySQL query statement below
--- select date_format(order_date,'%Y-%m') as month,count(order_id) as order_count, count(distinct customer_id) as customer_count
--- from Orders o
--- where invoice>20
--- group by date_format(order_date,'%Y-%m')
+# Write your MySQL query statement below
+select date_format(order_date,'%Y-%m') as month,count(order_id) as order_count, count(distinct customer_id) as customer_count
+from Orders o
+where invoice>20
+group by date_format(order_date,'%Y-%m')
